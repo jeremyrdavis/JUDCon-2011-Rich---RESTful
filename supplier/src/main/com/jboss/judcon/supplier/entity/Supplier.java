ce406425ce28088f34b415a67871d29adb83f95f
Jeremy Davis <jeremyrdavis@gmail.com>
2011-04-25 13:25:40 -0400

    Reading from xml post.  Forgot a few files in previous commit.

--- supplier/src/main/com/jboss/judcon/supplier/entity/Supplier.java
+++ supplier/src/main/com/jboss/judcon/supplier/entity/Supplier.java
@@ -6,46 +6,66 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Table;
 import javax.persistence.Version;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import org.hibernate.validator.Length;
 
 @Entity
-@Table(name="supplier")
-public class Supplier implements Serializable
-{
-	private int id;
-    private Integer version;
-    private String name;
+@Table(name = "supplier")
+@XmlRootElement(name="supplier")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Supplier implements Serializable {
 
-    
-    public Supplier() {
+	private static final long serialVersionUID = -2777750679855439742L;
+	
+	@XmlAttribute
+	private int id;
+	
+	@XmlElement
+	private Integer version;
+	
+	@XmlElement
+	private String name;
+
+	public Supplier() {
 		super();
 	}
+	
+	public Supplier(int idToSet, String nameToSet){
+		this.id = idToSet;
+		this.name = nameToSet;
+	}
 
-	@Id @GeneratedValue
-    public int getId() {
-        return id;
-    }
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    @Version
-    public Integer getVersion() {
-        return version;
-    }
+	@Version
+	public Integer getVersion() {
+		return version;
+	}
 
-    private void setVersion(Integer version) {
-        this.version = version;
-    }
+	private void setVersion(Integer version) {
+		this.version = version;
+	}
 
-    @Length(max = 20)
-    public String getName() {
-        return name;
-    }
+	@Length(max = 20)
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
--- supplier/src/main/com/jboss/judcon/supplier/services/SupplierService.java
+++ supplier/src/main/com/jboss/judcon/supplier/services/SupplierService.java
@@ -1,25 +1,78 @@
 package com.jboss.judcon.supplier.services;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
-import org.hibernate.type.TrueFalseType;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.log.Log;
 
-import com.jboss.judcon.supplier.utilities.MessageUtility;
+import com.jboss.judcon.supplier.entity.Supplier;
+import com.jboss.judcon.supplier.manager.SupplierMgr;
 
 @Name("SupplierService")
-@Path("/suppliers")
+@Path("/supplier")
 public class SupplierService {
-	
-	@In(create=true, value="MessageUtility")
-	MessageUtility messageUtility;
+
+	@Logger
+	private Log log;
+
+	@In(create = true, value = "SupplierMgr")
+	SupplierMgr supplierMgr;
 
 	@GET
-	@Produces(value = "text/plain")
-	public String helloWorld() {
-		return messageUtility.getDefaultMessage();
+	@Produces("application/xml")
+	@Path("{id}")
+	public Supplier getSupplierXml(@PathParam("id") int id) {
+
+		log.debug("getSupplierXml()");
+
+		return supplierMgr.getSupplier(id);
 	}
+
+	@GET
+	@Produces("application/json")
+	@Path("{id}")
+	public StreamingOutput getSupplierJson(@PathParam("id") int id) {
+
+		log.debug("getSupplierJson()");
+
+		final Supplier supplier = supplierMgr.getSupplier(id);
+
+		if (supplier == null) {
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+
+		return new StreamingOutput() {
+			public void write(OutputStream outputStream) throws IOException,
+					WebApplicationException {
+				ObjectMapper mapper = new ObjectMapper();
+				mapper.writeValue(outputStream, supplier);
+			}
+
+		};
+	}
+	
+	@POST
+	@Consumes("application/xml")
+	public Response createSupplier(Supplier supplier) {
+		Supplier retVal = supplierMgr.create(supplier);
+	   log.debug("Created customer " + retVal.getId(), null);
+	   return Response.created(URI.create("/customers/"
+	                                + supplier.getId())).build();
+	}
+
 }
